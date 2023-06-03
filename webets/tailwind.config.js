10340726b7bb9d861ad954475c6827ede2d02194
pgrwe <aldreichampadu@outlook.com>
2023-06-02 21:29:52 -0400

    working on colors

--- webets/tailwind.config.js
+++ webets/tailwind.config.js
@@ -7,10 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'paper_purple_dark': '#3b405e',
+        'paper_purple_light': '#4f3a54',
+        'paper_pink_cream': '#a4929a',
+        'paper_cream': '#e4dbba',
+
+        'foggy_purple-800': '#1e0721',
+        'foggy_purple-700': '#23132d',
+        'foggy_purple-600': '#26203a',
+        'foggy_slate-500': '#30324d',
+        'foggy_slate-400': '#444a65',
+        'foggy_slate-300': '#5d687c',
+        'foggy_slate-200': '#7c8893',
+        'foggy_slate-100': '#9ba9ab',
       },
     },
   },
