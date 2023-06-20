a94247e7346bdfcd2bdcb0144c9a857f6f8bdf8e
pgrwe <aldreichampadu@outlook.com>
2023-06-20 09:53:12 -0400

    working on hours

--- webets/app/about/page.tsx
+++ webets/app/about/page.tsx
@@ -6,9 +6,7 @@ export default function About() {
     <div className='min-h-screen pt-16 flex flex-col mx-4 justify-center 
     drop-shadow-lg text-e6palette-200'>
         About
-      <div className='fixed bottom-0 w-full flex flex-col items-center justify-center'>
-        <FaChevronDown size={35} className='animate-bounce'/>
-      </div>
+      <FaChevronDown size={35} className='fixed bottom-0 w-full flex flex-col items-center justify-center animate-bounce'/>
     </div>
   )
 }
