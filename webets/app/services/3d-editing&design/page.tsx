95769087486b8c10a0b8455a60c52029a328925e
pgrwe <aldreichampadu@outlook.com>
2023-06-22 02:28:42 -0400

    fixing components

---
+++ webets/app/services/3d-editing&design/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='min-h-screen pt-16 flex flex-col mx-4 justify-center 
+    drop-shadow-lg text-e6palette-200'>
+        About
+    </div>
+  )
+}
