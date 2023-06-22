25b2a9b31ba08a12781b2ce934b87a8f0fd9f869
pgrwe <aldreichampadu@outlook.com>
2023-06-22 00:37:05 -0400

    frontpage near complete

--- webets/app/components/navbar.tsx
+++ webets/app/components/navbar.tsx
@@ -23,18 +23,25 @@ export default function NavBar() {
   return (
     <nav 
       className='p-4 md:p-6 fixed w-screen flex flex-row justify-start items-center z-40'>
-      <Link href='/' className=' drop-shadow-xl'>
-        <Image src='/etslogo1cut.png' width={60} height={0} alt = 'logo' className='hover:scale-95 hover:border-x transition-all duration-400'  />
+      <Link href='/' className='drop-shadow-xl'>
+        <Image src='/etslogo1cut.png' width={60} height={0} alt = 'logo' className='hover:scale-95 transition-all duration-430'  />
       </Link>
+
       <div className='hidden ml-auto pr-6 font-display text-xl md:flex flex-row gap-16 text-e6palette-800'>
-        <Link href='/about' className='hover:underline drop-shadow-xl'>
-          About
+
+        <Link href='/about' className="group">
+          <div className="drop-shadow-xl">About</div>
+          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-e6palette-800"></span>
         </Link>
-        <Link href='/services' className='hover:underline drop-shadow-xl'>
-          Services
+
+        <Link href='/about' className="group">
+          <div className="drop-shadow-xl">Services</div>
+          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-e6palette-800"></span>
         </Link>
-        <Link href='/services' className='hover:underline drop-shadow-xl'>
-          Contact
+
+        <Link href='/about' className="group">
+          <div className="drop-shadow-xl">Contact</div>
+          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-e6palette-800"></span>
         </Link>
       </div>
       {/* <div className='flex flex-row md:hidden ml-auto'>
